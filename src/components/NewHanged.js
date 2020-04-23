5c3efd0c43dccf90c3cca4ef3d0f655e697852b2
jerkodeur <jerome.potie@gmail.com>
2020-04-23 20:28:09 +0200

    Call to expressions's Json file

--- src/components/NewHanged.js
+++ src/components/NewHanged.js
@@ -1,19 +1,27 @@
 import React from 'react'
 
 import './NewHanged.css'
+import data from './expressions.json'
+
+const { id, name, description } = data.expressions[0]
 
 class NewHanged extends React.Component {
+
   constructor(props) {
     super(props)
     this.state = {
       attempts: 0,
       useLetters: [],
       remainingLetters: "ABCDEFGHIJKLMNOPKRSTUVWXYZ",
-      expression: "",
+      expression: name,
       lettersToFind: ""
     }
     this.handleClick = this.handleClick.bind(this)
     this.checkMatching = this.checkMatching.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({ expression: name })
   }
 
   handleClick() { }
@@ -21,6 +29,7 @@ class NewHanged extends React.Component {
   checkMatching() { }
 
   render() {
+    //   console.log(name, this.state.expression)
     return (
       <div>test</div>
     )
